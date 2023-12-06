c6aaf18b4cf783a535db3aa2bd6111f64dbd2daa
Ying Quan <quanying26@hotmail.com>
2023-12-06 00:11:36 -0500

    add error handling to product route

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 dotenv.config();
 import connectDB from "./config/db.js";
 
@@ -13,7 +14,10 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
