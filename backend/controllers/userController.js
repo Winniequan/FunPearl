2c3ac4fb300d0bfe45702debbc5e6332fc1fc190
Ying Quan <quanying26@hotmail.com>
2023-12-09 18:34:47 -0500

    get user profile function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,7 @@ const authUser = asyncHandler(async (req, res) => {
 
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -48,7 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
+      isAdmin: user.isAdmin
     });
   } else {
     res.status(400);
@@ -72,7 +72,21 @@ const logoutUser = asyncHandler(async (req, res) => {
 //route  GET/api/users/profile
 //access private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send(" user profile");
+  const user = await User.findById(req.user._id);
+
+  if(user){
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      isAdmin: user.isAdmin
+    })
+
+  }else{
+    res.status(404);
+    throw new Error("User not found")
+  }
 });
 
 //update user profile
