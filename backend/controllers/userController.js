e7bca787e3812d98d5185f86bbe0754b9b1513d5
Ying Quan <quanying26@hotmail.com>
2023-12-09 15:36:51 -0500

    set up jwtwebtoken and cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,27 +1,37 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 //auth user & get token
 //route  POST/api/users/login
 //access Public
 const authUser = asyncHandler(async (req, res) => {
-  
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email });
 
-  if(user && (await user.matchPassword(password))){
+  if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
+      expiresIn: "30d"
+    }) // create token
+
+    //Set JWT as HTTP-Only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",// true in production
+      sameSite: "strict",
+      maxAge: 30*24*60*60*1000 // 30days
+    })
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin
-   })
+      isAdmin: user.isAdmin,
+    });
   } else {
     res.status(401);
-    throw new Error("Invalid email or password")
+    throw new Error("Invalid email or password");
   }
- 
 });
 
 //REGISTER user
