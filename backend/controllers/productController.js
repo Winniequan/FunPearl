6b1baf597767bda70c8d0cd5b840198675533c0e
Ying Quan <quanying26@hotmail.com>
2023-12-20 09:56:57 -0500

    create product reviews backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -26,62 +26,109 @@ const getProductById = asyncHandler(async (req, res) => {
 //route  POST/api/products
 //access Public
 const createProduct = asyncHandler(async (req, res) => {
- const product = new Product({
-  name: "Sample name",
-  price: 0,
-  user: req.user._id,
-  image: "/image/sample.jpg",
-    brand: 'Sample brand',
-    category: 'Sample category',
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/image/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
     countInStock: 0,
     numReviews: 0,
-    description: 'Sample description',
+    description: "Sample description",
   });
 
   const createdProduct = await product.save();
   res.status(201).json(createdProduct);
- })
+});
 
- //update a product
+//update a product
 //route  put/api/products/:id
 //access Private/ Admin
 const updateProduct = asyncHandler(async (req, res) => {
-   const { name, price, description, image, brand, category, countInStock } =
-     req.body;
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
 
-   const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
 
-   if (product) {
-     product.name = name;
-     product.price = price;
-     product.description = description;
-     product.image = image;
-     product.brand = brand;
-     product.category = category;
-     product.countInStock = countInStock;
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
 
-     const updatedProduct = await product.save();
-     res.json(updatedProduct);
-   } else {
-     res.status(404);
-     throw new Error("Product not found");
-   }
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
 });
 
- //delete a product
+//delete a product
 //route  delete/api/products/:id
 //access Private/ Admin
 const deleteProduct = asyncHandler(async (req, res) => {
-   const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
 
-   if (product) {
-     await Product.deleteOne({_id: product._id});
-     res.status(200).json({message: "Product Deleted"})
-   } else {
-     res.status(404);
-     throw new Error("Product not found");
-   }
+  if (product) {
+    await Product.deleteOne({ _id: product._id });
+    res.status(200).json({ message: "Product Deleted" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
 });
 
+//create a new review
+//route  post/api/products/:id/reviews
+//access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
 
-export { getProducts, getProductById, createProduct, updateProduct, deleteProduct}
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview
+};
