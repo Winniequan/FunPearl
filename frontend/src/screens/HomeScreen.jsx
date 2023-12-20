1978782e1f7adea99dbea186b8f7fda26f0a2121
Ying Quan <quanying26@hotmail.com>
2023-12-20 10:24:41 -0500

    paginate products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Product from "../components/Product";
@@ -6,7 +7,8 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const {pageNumber} = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({pageNumber});
 
   return (
     <>
@@ -20,7 +22,7 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
